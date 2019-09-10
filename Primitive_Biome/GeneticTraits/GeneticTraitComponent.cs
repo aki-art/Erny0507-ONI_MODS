c93312d0ff157b6d47bdc7d9d09058834344dde3
erny0507 <erny_0507@hotmail.com>
2019-09-10 12:23:45 -0500

    Update GeneticTraitComponent.cs

--- Primitive_Biome/GeneticTraits/GeneticTraitComponent.cs
+++ Primitive_Biome/GeneticTraits/GeneticTraitComponent.cs
@@ -76,7 +76,73 @@ private static readonly EventSystem.IntraObjectHandler<CritterName> OnSpawnedFro
     {
       return this.HasTag(GameTags.Egg);
     }
-    
+     public void SetName(string newName)
+    {
+      generation = 1;
+      if (Util.IsNullOrWhitespace(newName))
+      {
+        ResetToPrefabName();
+        return;
+      }
+
+      critterName = newName;
+      ApplyName();
+    }
+
+    private void setGameObjectName(string newName)
+    {
+      KSelectable selectable = GetComponent<KSelectable>();
+
+      name = newName;
+      selectable?.SetName(newName);
+      gameObject.name = newName;
+    }
+
+    public void ApplyName()
+    {
+      if (!IsCritter()) return;
+
+      string newName = critterName;
+      if (generation == 2)
+      {
+        newName += " Jr.";
+      }
+      else if (generation > 2)
+      {
+        newName += " " + Util.ToRoman(generation);
+      }
+      setGameObjectName(newName);
+    }
+
+    public bool HasName()
+    {
+      return !Util.IsNullOrWhitespace(critterName);
+    }
+
+    public void TransferTo(CritterName other)
+    {
+      if (other == null || !HasName()) return;
+
+      other.critterName = critterName;
+      other.generation = generation;
+
+      if (other.IsEgg())
+      {
+        other.generation += 1;
+      }
+
+      other.ApplyName();
+    }
+
+    public void ResetToPrefabName()
+    {
+      KPrefabID prefab = GetComponent<KPrefabID>();
+      if (prefab != null)
+      {
+        critterName = "";
+        setGameObjectName(TagManager.GetProperName(prefab.PrefabTag));
+      }
+    }
     }
 }
 
