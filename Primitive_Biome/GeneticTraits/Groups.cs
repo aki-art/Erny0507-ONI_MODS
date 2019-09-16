0bae057bc168bc500777a0bd96441af996fda728
erny0507 <erny_0507@hotmail.com>
2019-09-16 17:23:03 -0500

    Update Groups.cs

--- Primitive_Biome/GeneticTraits/Groups.cs
+++ Primitive_Biome/GeneticTraits/Groups.cs
@@ -15,9 +15,12 @@ namespace Primitive_Biome.GeneticTraits
     public static readonly Group LifespanGroup = new Group("LifespanGroup", 0.15f, inst => HasAmount(inst, Db.Get().Amounts.Age));
     public static readonly Group FertilityGroup = new Group("FertilityGroup", 0.1f, inst => HasAmount(inst, Db.Get().Amounts.Fertility));
     
-    //public static readonly Group EggHatchingSpeedGroup = new Group("EggHatchingSpeedGroup", 0.05f);
-     //public static readonly Group GrowingUpSpeedGroup = new Group("GrowingUpSpeedGroup", 0.05f);
-
+    public static readonly Group ElementEmitterGroup = new Group("ElementEmitterGroup", 0.1f);
+    public static readonly Group ElementAbsorberGroup = new Group("ElementAbsorberGroup", 0.1f);
+    public static readonly Group ElementConverterGroup = new Group("ElementConverterGroup", 0.1f);
+    public static readonly Group EggHatchingSpeedGroup = new Group("EggHatchingSpeedGroup", 0.05f);
+    public static readonly Group GrowingUpSpeedGroup = new Group("GrowingUpSpeedGroup", 0.05f);
+    public static readonly Group HealthGroup = new Group("HealthGroup", 0.05f);
     public Group(string id, float probability, Predicate<GameObject> requirement = null,List<string> exceptions=null )
     {
       Id = id;
