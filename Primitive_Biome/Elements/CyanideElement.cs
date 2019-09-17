3e5d986f510f435181a60d95a4f1527b4b97a569
erny0507 <erny_0507@hotmail.com>
2019-09-17 13:01:13 -0500

    Update CyanideElement.cs

--- Primitive_Biome/Elements/CyanideElement.cs
+++ Primitive_Biome/Elements/CyanideElement.cs
@@ -10,8 +10,33 @@ namespace Primitive_Biome.Elements
     {
         public static string ID = "Cyanide";
         public static readonly Color32 CYANIDE_FADED_YELLOW = new Color32(242, 206, 82, 255);
-        public static readonly SimHashes BloodSimHash = (SimHashes)Hash.SDBMLower("Blood");
-        public static readonly SimHashes FrozenBloodSimHash = (SimHashes)Hash.SDBMLower("FrozenBlood");
- 
+        //public static readonly SimHashes BloodSimHash = (SimHashes)Hash.SDBMLower("Blood");
+        //public static readonly SimHashes FrozenBloodSimHash = (SimHashes)Hash.SDBMLower("FrozenBlood");
+ public const string Data = @"elements:
+  - elementId: Cyanide
+    specificHeatCapacity: 1
+    thermalConductivity: 2
+    solidSurfaceAreaMultiplier: 1
+    liquidSurfaceAreaMultiplier: 1
+    gasSurfaceAreaMultiplier: 1
+    strength: 0.1
+    highTemp: 2000
+    highTempTransitionTarget: MoltenAluminum
+    defaultTemperature:  290
+    defaultMass: 700
+    maxMass: 2000
+    hardness: 5
+    molarMass: 26.02
+    lightAbsorptionFactor: 1
+    materialCategory: ConsumableOre
+    buildMenuSort: 5
+    isDisabled: false
+    state: Solid
+    localizationID: STRINGS.ELEMENTS.CYANIDE.NAME";
+
+        public const string Id = "Cyanide";
+        public static string Name = UI.FormatAsLink("Cyanide", Id.ToUpper());
+        public static string Description = $"Cyanide in its purest form. Not safe to eat.";
+        public static SimHashes SimHash = (SimHashes)Hash.SDBMLower(Id);
     }
 }
