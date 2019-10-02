fb53cc9ed051e7f31189e2f26eb51dd0f54620b2
erny0507 <erny_0507@hotmail.com>
2019-10-02 12:28:37 -0500

    Create ElementConverterComponent.cs

---
+++ Primitive_Biome/GeneticTraits/Traits/ElementConverterComponent.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KSerialization;
+using UnityEngine;
+namespace Primitive_Biome.GeneticTraits.Traits
+{
+    [SerializationConfig(MemberSerialization.OptIn)]
+    class ElementConverterComponent : KMonoBehaviour, ISaveLoadable
+
+    {
+    
+    }
+ }
