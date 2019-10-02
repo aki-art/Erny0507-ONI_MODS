c72cd47a6e73925749117101e55921d7c7885a89
erny0507 <erny_0507@hotmail.com>
2019-10-02 13:03:38 -0500

    Update ElementConverterComponent.cs

--- Primitive_Biome/GeneticTraits/Traits/ElementConverterComponent.cs
+++ Primitive_Biome/GeneticTraits/Traits/ElementConverterComponent.cs
@@ -10,6 +10,33 @@ namespace Primitive_Biome.GeneticTraits.Traits
     class ElementConverterComponent : KMonoBehaviour, ISaveLoadable
 
     {
-    
+    [Serialize]
+        public bool isSet=false;
+         [Serialize]
+    public SimHashes element_input;
+     [Serialize]
+        public SimHashes element_output;
+  protected override void OnPrefabInit()
+        {
+            base.OnPrefabInit();
+
+        }
+        protected void OnSpawn(GameObject go)
+        {
+        if(!isSet){
+        randomizeColor()
+        isSet=true;
+        }
+               UtilPB.ApplyTint(go, color);
+        }
+        protected override void OnCleanUp()
+        {
+            base.OnCleanUp();
+        }
+        private void setConfiguration(){
+       var colors=OffColor.colors;
+        Util.Shuffle(colors);
+                    color = colors.First();
+        }
     }
- }
+}
